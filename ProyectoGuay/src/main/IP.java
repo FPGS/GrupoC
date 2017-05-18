47aa922037c2f6534ae2ead726604abb863429c8
MocoNinja <javiergonzaleztello@gmail.com>
2017-05-18 19:09:09 +0200

    Metidas las funcionalidades fundamentales de redes.
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

---
+++ ProyectoGuay/src/main/IP.java
@@ -0,0 +1,65 @@
+import java.net.*;
+
+/*
+* Objetivos de la clase:
+* 	Conseguir la IP local
+* 	Conseguir la máscara de red
+* 	*** EXTRAS PARA AÑADIR *** 
+* 	Conseguir la MAC
+*/
+public class IP {
+	private InetAddress localhost;
+	private String localHostAdress, subnetMask;
+
+	public IP() throws UnknownHostException {
+		setLocalHost();
+		setLocalHostAdress();
+		setSubnetMask();
+	}
+
+	private void setLocalHost() throws UnknownHostException {
+		this.localhost = Inet4Address.getLocalHost();
+	}
+
+	private void setLocalHostAdress() throws UnknownHostException {
+		this.localHostAdress = InetAddress.getLocalHost().getHostAddress();
+	}
+
+	private void setSubnetMask() {
+		String range = this.localHostAdress.substring(0, 2);
+		int rangeValue = Integer.parseInt(range);
+		if (rangeValue >= 0 && rangeValue <= 127) {
+			subnetMask = "255.0.0.0";
+		} else if (rangeValue >= 128 && rangeValue <= 191) {
+			subnetMask = "255.255.0.0";
+		} else if (rangeValue >= 192 && rangeValue <= 223) {
+			subnetMask = "255.255.255.0";
+		} else {
+			subnetMask = "undefined";
+		}
+	}
+
+	public void readLocalHost() {
+		System.out.println(localhost);
+	}
+
+	public void readLocalHostAdress() {
+		System.out.println(localHostAdress);
+	}
+
+	public void readSubnetMask(){
+		System.out.println(subnetMask);
+	}
+	
+	public InetAddress getLocalHost() {
+		return localhost;
+	}
+
+	public String getLocalHostAdress() {
+		return localHostAdress;
+	}
+
+	public String getSubnetMask(){
+		return subnetMask;
+	}
+}
