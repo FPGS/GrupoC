25ac6390b38d0cf46cc3ce7416d2ac655f217ab1
root <root@JaviDesktopWin.localdomain>
2017-05-21 11:14:16 +0200

    Configurado Maven y sql-connector
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

---
+++ controlAsistencia/src/main/java/grupoC/controlAsistencia/APP.java
@@ -0,0 +1,15 @@
+package grupoC.controlAsistencia;
+
+import java.net.UnknownHostException;
+
+public class APP {
+
+	public static void main(String[] args) {
+		try {
+			IP ip = new IP();
+			ip.readLocalHost();
+			ip.readLocalHostAdress();
+			ip.readSubnetMask();
+		} catch (UnknownHostException e) { e.printStackTrace(); }		
+	}
+}
