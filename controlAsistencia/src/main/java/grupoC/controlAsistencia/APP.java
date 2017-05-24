cae59d7479f74a6c6c23c7415c8d044de721f0cc
David Izquierdo <izquierdoboscosgmail.com>
2017-05-24 13:37:56 +0200

    añadido el readMacAdress al proyecto maven.

--- controlAsistencia/src/main/java/grupoC/controlAsistencia/APP.java
+++ controlAsistencia/src/main/java/grupoC/controlAsistencia/APP.java
@@ -1,16 +1,18 @@
 package grupoC.controlAsistencia;
 
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.sql.SQLException;
 
 public class APP {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, SocketException {
 		try {
 			IP ip = new IP();
 			ip.readLocalHost();
 			ip.readLocalHostAdress();
 			ip.readSubnetMask();
+			ip.readMacAdress();
 		} catch (UnknownHostException e) { e.printStackTrace(); }		
 		
 		Registros con = new Registros();
